151d03e95d70a2aacd61c0547ef9aee30df1ca93
alpaz <91884588+alpazified@users.noreply.github.com>
2023-08-19 15:28:25 -0400

    feat: delete commit files

--- src/selectMenus/example/example.ts
+++
@@ -1,8 +0,0 @@
-import { SelectMenu } from "../../structures/selectMenu";
-
-export default new SelectMenu({
-    customId: 'example',
-    execute: async ({ interaction }) => {
-        interaction.reply({ content: `Example select menu.` });
-    }
-})
